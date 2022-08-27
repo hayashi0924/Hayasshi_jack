5189a408c50c69192c5e58f24c68aa9c22e8dd7d
hayashi <hayashi.takuma0924@gmail.com>
2022-08-27 10:02:53 +0900

    判定クラスの経過報告（Playerクラスの実装が必要です）

---
+++ Hayasshi_jack/src/rule/Judgement.java
@@ -0,0 +1,14 @@
+package rule;
+
+import Card.Card;
+
+public class Judgement {
+    private final int UPPER_LIMIT = 21;
+
+    public static void ofNumber(Player charender, Player dealar){
+        ;
+    }
+    public static boolean isValid(Player player){
+        return ;
+    }
+}
